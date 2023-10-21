5be972987482d8d6a481e5f0ddfd5844eefd6681
anton <grasarade@gmail.com>
2023-10-21 19:52:09 +0200

    Completed preprocessing pipeline with replace_nan_to_value

--- util/preprocessing.py
+++ util/preprocessing.py
@@ -1,59 +1,55 @@
-from typing import Union, Dict, List, Callable
+from typing import Union, Dict, List, Callable, Tuple
 from util.features_info import Feature, FeatureType, FEATURES_DICT, REPLACEMENT_DICT
 
 import numpy as np
 
 
-def remove_nans(x: np.ndarray,
-                where: Union[str, List[str]],
-                feature_index: Dict[str, int]) -> np.ndarray:
-    """
-    Call set_nans_to_value for all the specified features using the correct value,
-    computed here if needed (for example for mean or median).
-    Note: not inplace, dataset is copied!
+def preprocessing_pipeline(x: np.ndarray,
+                           where: Union[str, List[str]],
+                           feature_index: Dict[str, int],
+                           nan_replacement: List[Tuple[List[str], Union[float, str]]] = None) -> np.ndarray:
+    prepro_df = align_nans(map_values(x, where, feature_index), where, feature_index)
+    if nan_replacement is not None:
+        for (features_list, val) in nan_replacement:
+            prepro_df = set_nans_to_value(prepro_df, value=val, where=features_list, feature_index=feature_index)
 
-    :param x: dataset
-    :param where: feature(s) where to apply the mapping operation
-    :param feature_index: dictionary that maps feature names to the (column) index in the dataset x
-    :return: (new) dataset after the preprocessing
-    """
-    # TODO: this method must be called after the align_nans method
-    # NOTE: with this implementation the dataset may cause multiple copies
-    # because of multiple calls to set_nans_to_value. Solve this issue.
-    if type(where) == str:
-        where = [where]
-
-    for f in feature_index.keys():
-        value = REPLACEMENT_DICT[f]
-        if type(value) == str:
-            if value == 'mean':
-                value = np.nanmean(x[:, feature_index[f]])
-            elif value == 'median':
-                value = np.nanmedian(x[:, feature_index[f]])
-        elif type(value) == int:
-            pass
-        else:
-            raise ValueError(f"Unknown value {value} for feature {f}")
-
-        x = set_nans_to_value(x, value, f, feature_index)
-
-    return x
+    return prepro_df
 
 
-def set_nans_to_value(x: np.ndarray, value: int, where: Union[str, List[str]],
+
+def set_nans_to_value(x: np.ndarray, value: Union[float, str], where: Union[str, List[str]],
                       feature_index: Dict[str, int]) -> np.ndarray:
     """
     Maps all the nan values to a specified value.
     Note: not inplace, dataset is copied.
 
     :param x: dataset
-    :param value: replacement value for nan
+    :param value: replacement value for nan, or alternatively "mean", "median"
     :param where: feature(s) where to apply the mapping operation
     :param feature_index: dictionary that maps feature names to the (column) index in the dataset x
     :return: (new) dataset after the preprocessing
     """
-    vectorized_nans_set = np.vectorize(lambda feature, v: value if np.isnan(v) else v)
-    return _apply_preprocessing(x, where, feature_index, vectorized_operation=vectorized_nans_set)
+
+    def nan_replacer(feature: Feature, v: np.ndarray):
+        """
+        Replaces nan values in v (value is given)
+        :param feature: unused
+        :param v: 1D array, column where to replace nan values
+        :return: column with replaced nan values
+        """
+        if type(value) == str:
+            if value == "mean":
+                replacement = np.nanmean(v)
+            elif value == "median":
+                replacement = np.nanmedian(v)
+            else:
+                raise AssertionError("Invalid value for replacement")
+        else:
+            replacement = value
+
+        return np.nan_to_num(v, nan=replacement)
+
+    return _apply_preprocessing(x, where, feature_index, vectorized_operation=nan_replacer)
 
 
 def align_nans(x: np.ndarray, where: Union[str, List[str]], feature_index: Dict[str, int]) -> np.ndarray:
