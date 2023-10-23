4b95bf73ee1a73e724b42a26469500dd466c739a
anton <grasarade@gmail.com>
2023-10-23 18:52:33 +0200

    Added evaluation summary

--- util/evaluation.py
+++ util/evaluation.py
@@ -4,8 +4,8 @@ def accuracy(true_labels: np.ndarray,
              predicted_labels: np.ndarray) -> float:
     """
     Calculate the accuracy of a classifier.
-    :param true_labels (numpy.ndarray): True class labels.
-    :param predicted_labels (numpy.ndarray): Predicted class labels.
+    :param true_labels: True class labels.
+    :param predicted_labels: Predicted class labels.
     :return float: Accuracy of the classifier.
     """
     if true_labels.shape != predicted_labels.shape:
@@ -16,29 +16,12 @@ def accuracy(true_labels: np.ndarray,
     return correct_predictions / total_samples
 
 
-def f1_score(true_labels: np.ndarray,
-             predicted_labels: np.ndarray) -> float:
-    """
-    Calculate the F1 score of a classifier.
-    :param true_labels (numpy.ndarray): True class labels.
-    :param predicted_labels (numpy.ndarray): Predicted class labels.
-    :return float: F1 score of the classifier.
-    """
-    if true_labels.shape != predicted_labels.shape:
-        raise ValueError("Input arrays must have the same shape.")
-    
-    precision = precision(true_labels, predicted_labels)
-    recall = recall(true_labels, predicted_labels)
-    
-    return 2 * (precision * recall) / (precision + recall)
-    
-
 def precision(true_labels: np.ndarray,
               predicted_labels: np.ndarray) -> float:
     """
     Calculate the precision of a classifier.
-    :param true_labels (numpy.ndarray): True class labels.
-    :param predicted_labels (numpy.ndarray): Predicted class labels.
+    :param true_labels: True class labels.
+    :param predicted_labels: Predicted class labels.
     :return float: precision of the classifier.
     """
     true_positives = np.sum((true_labels == 1) & (predicted_labels == 1))
@@ -51,11 +34,36 @@ def recall(true_labels: np.ndarray,
            predicted_labels: np.ndarray) -> float:
     """
     Calculate the recall of a classifier.
-    :param true_labels (numpy.ndarray): True class labels.
-    :param predicted_labels (numpy.ndarray): Predicted class labels.
+    :param true_labels: True class labels.
+    :param predicted_labels: Predicted class labels.
     :return float: recall of the classifier.
     """
     true_positives = np.sum((true_labels == 1) & (predicted_labels == 1))
     false_negatives = np.sum((true_labels == 1) & (predicted_labels == -1))
 
     return true_positives / (true_positives + false_negatives)
+
+
+def f1_score(true_labels: np.ndarray,
+             predicted_labels: np.ndarray) -> float:
+    """
+    Calculate the F1 score of a classifier.
+    :param true_labels: True class labels.
+    :param predicted_labels: Predicted class labels.
+    :return float: F1 score of the classifier.
+    """
+    if true_labels.shape != predicted_labels.shape:
+        raise ValueError("Input arrays must have the same shape.")
+
+    prec = precision(true_labels, predicted_labels)
+    rec = recall(true_labels, predicted_labels)
+
+    return 2 * (prec * rec) / (prec + rec)
+
+
+def evaluation_summary(true_labels: np.ndarray,
+                       predicted_labels: np.ndarray):
+    print(f"Accuracy: {accuracy(true_labels, predicted_labels):.2%}")
+    print(f"Precision: {precision(true_labels, predicted_labels):.2%}")
+    print(f"Recall: {recall(true_labels, predicted_labels):.2%}")
+    print(f"F1-score: {f1_score(true_labels, predicted_labels):.2%}")
