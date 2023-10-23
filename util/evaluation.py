2c467e75e1335cd7eecb497f973a2a9167b3ff5c
Matteo Santelmo <matteo.santelmo@gmail.com>
2023-10-23 17:27:23 +0200

    Added new metrics (precision and recall)

--- util/evaluation.py
+++ util/evaluation.py
@@ -27,11 +27,35 @@ def f1_score(true_labels: np.ndarray,
     if true_labels.shape != predicted_labels.shape:
         raise ValueError("Input arrays must have the same shape.")
     
-    true_positives = np.sum((true_labels == 1) & (predicted_labels == 1))
-    false_positives = np.sum((true_labels == -1) & (predicted_labels == 1))
-    false_negatives = np.sum((true_labels == 1) & (predicted_labels == -1))
+    precision = precision(true_labels, predicted_labels)
+    recall = recall(true_labels, predicted_labels)
     
-    precision = true_positives / (true_positives + false_positives)
-    recall = true_positives / (true_positives + false_negatives)
     return 2 * (precision * recall) / (precision + recall)
     
+
+def precision(true_labels: np.ndarray,
+              predicted_labels: np.ndarray) -> float:
+    """
+    Calculate the precision of a classifier.
+    :param true_labels (numpy.ndarray): True class labels.
+    :param predicted_labels (numpy.ndarray): Predicted class labels.
+    :return float: precision of the classifier.
+    """
+    true_positives = np.sum((true_labels == 1) & (predicted_labels == 1))
+    false_positives = np.sum((true_labels == -1) & (predicted_labels == 1))
+
+    return true_positives / (true_positives + false_positives)
+
+
+def recall(true_labels: np.ndarray,
+           predicted_labels: np.ndarray) -> float:
+    """
+    Calculate the recall of a classifier.
+    :param true_labels (numpy.ndarray): True class labels.
+    :param predicted_labels (numpy.ndarray): Predicted class labels.
+    :return float: recall of the classifier.
+    """
+    true_positives = np.sum((true_labels == 1) & (predicted_labels == 1))
+    false_negatives = np.sum((true_labels == 1) & (predicted_labels == -1))
+
+    return true_positives / (true_positives + false_negatives)
