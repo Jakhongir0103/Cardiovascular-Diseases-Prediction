adc57ef2818959dfaebf3a72fb016eccce1fdd18
anton <grasarade@gmail.com>
2023-10-25 18:22:27 +0200

    Updated grid search

--- util/train.py
+++ util/train.py
@@ -1,3 +1,6 @@
+import itertools
+from typing import Dict, List
+
 import numpy as np
 
 from implementations import logistic_loss, logistic_loss_gradient, batch_iter
@@ -13,7 +16,8 @@ def reg_logistic_regression(
     max_iter: int, 
     gamma: float, 
     batch_size: int = 1, 
-    all_losses: bool = False
+    all_losses: bool = False,
+    optimizer: str = "sgd"
 ):
     """
     Regularized logistic regression using SGD.
@@ -30,6 +34,7 @@ def reg_logistic_regression(
         gamma: an array of gammas
         batch_size: scalar
         all_losses: bool, if set to True returns an array of losses, otherwise returns the loss(scalar) of optimum w
+        optimizer: either "sgd", "adagrad" or "adam"
 
     Returns:
         w: numpy array, optimal weights.
@@ -37,43 +42,67 @@ def reg_logistic_regression(
                       if all_losses is True -> numpy array, train losses for each iteration                      
         valid_losses: if all_losses is False -> scalar, validation loss corresponding to the optimum w
                       if all_losses is True -> numpy array, validation losses for each iteration
-    """    
+    """
+    assert optimizer in ["sgd", "adam", "adagrad"]
+
+    if optimizer == "adagrad":
+        gradients_sum = 0
+    elif optimizer == "adam":
+        gradients_sum = np.zeros(shape=(tx_train.shape[1], 1))
+
     if all_losses:
         train_losses = [logistic_loss(y_train, tx_train, w, lambda_)]
         valid_losses = [logistic_loss(y_valid, tx_valid, w, lambda_)]
 
-    for _ in range(max_iter):
-        # if _ % 200 == 0:
-        #     print(f"Iteration {_}/{max_iter}")
+
+    best_w = w
+    lowest_loss = np.inf
+
+    for it in range(max_iter):
+        if it % 200 == 0:
+            print(f"Iteration {it}/{max_iter} -> lowest loss {lowest_loss}")
 
         for batch_y, batch_x in batch_iter(y_train, tx_train, batch_size, shuffle=True):
             gradient = logistic_loss_gradient(batch_y, batch_x, w, lambda_)
-            w = w - gamma * gradient
+
+            if optimizer == "sgd":
+                w = w - gamma * gradient
+            elif optimizer == "adagrad":
+                gradients_sum += np.dot(gradient, gradient)
+                h = np.sqrt(gradients_sum)  # h is scalar
+                w = w - (1/h) * gamma  * gradient
+            elif optimizer == "adam":
+                beta = 0.9
+                gradients_sum += beta * gradients_sum + (1 - beta) * (gradient * gradient)
+                h_vect = np.sqrt(gradients_sum/(1 - beta**it))
+                w = w - (1/h_vect) * gamma * gradient
+
+            valid_loss = logistic_loss(y_valid, tx_valid, w, lambda_)
+            if valid_loss < lowest_loss:
+                lowest_loss = valid_loss
+                best_w = w
 
         if all_losses: 
             train_losses.append(logistic_loss(y_train, tx_train, w, lambda_))
             valid_losses.append(logistic_loss(y_valid, tx_valid, w, lambda_))
 
     if not all_losses:    
-        train_losses = logistic_loss(y_train, tx_train, w, lambda_)
-        valid_losses = logistic_loss(y_valid, tx_valid, w, lambda_)
+        train_losses = logistic_loss(y_train, tx_train, best_w, lambda_)
+        valid_losses = lowest_loss
 
-    return w, train_losses, valid_losses
+    return best_w, train_losses, valid_losses
 
 
 def reg_logistic_regression_hyperparameters(
     x_train: np.ndarray, 
     y_train: np.ndarray, 
     x_valid: np.ndarray, 
-    y_valid: np.ndarray, 
-    lambdas: np.ndarray, 
-    gammas: np.ndarray, 
-    w_initial: np.ndarray, 
-    max_iter: int = 5000, 
-    batch_size: int = 1
-):
+    y_valid: np.ndarray,
+    hyperpar_grid: Dict[str, List],
+    max_iter: int = 5000,
+) -> List[Dict]:
     """
-    GridSearchCV for regularized logistic regression. 
+    GridSearch for regularized logistic regression.
     Return the optimum w and corresponding train loss, validation loss, lambda, gamma.
 
     Args:
@@ -81,53 +110,38 @@ def reg_logistic_regression_hyperparameters(
         y_train:  shape=(N, 1)
         x_valid:  shape=(N, D)
         y_valid:  shape=(N, 1)
-        lambdas: an array of lambdas
-        gammas: an array of gammas
-        w_initial: an array of initial values for w
+        hyperpar_grid: Dict which contains keys "lambda_", "gamma", "batch_size", "optimizer" and whose values
+        are lists of possible values for the hyperparameter
         max_iter: scalar
-        batch_size: scalar
-
-    Returns:
-        best_w: numpy array, optimal weights.
-        best_train_losses: scalar, train loss corresponding to the best_w
-        best_valid_losses: scalar, validation loss corresponding to the best_w
-        best_lambda: scalar, best value for lambda
-        best_gamma: scalar, best value for gamma
+    :return: List of Dict containing hyperparameter settings and results
     """    
-    train_losses_all = []
-    valid_losses_all = []
-    w_all = []
 
-    for lambda_ in lambdas:
-        for gamma in gammas:
-            # Perform logistic regression with the current hyperparameters
-            w, train_losses, valid_losses = reg_logistic_regression(
+    def cartesian_product(hyperpar_dict: Dict[str, List]):
+        return [dict(zip(hyperpar_dict.keys(), values)) for values in itertools.product(*hyperpar_dict.values())]
+
+    hyp_params_settings: List = cartesian_product(hyperpar_dict=hyperpar_grid)
+    for hyp_params in hyp_params_settings:
+        
+        assert "lambda_" in hyp_params
+        assert "gamma" in hyp_params
+        assert "batch_size" in hyp_params
+        assert "optimizer" in hyp_params
+
+        w, train_loss, valid_loss = reg_logistic_regression(
                 tx_train=x_train, 
                 y_train=y_train, 
                 tx_valid=x_valid, 
                 y_valid=y_valid, 
-                lambda_=lambda_, 
+                lambda_=hyp_params["lambda_"],
                 max_iter=max_iter, 
-                gamma=gamma, 
-                batch_size=batch_size, 
-                w=w_initial
+                gamma=hyp_params["gamma"],
+                batch_size=hyp_params["batch_size"],
+                w=np.random.random(size=x_train.shape[1]),
+                optimizer=hyp_params["optimizer"],
+                all_losses=False
             )
-            w_all.append(w)
-            train_losses_all.append(train_losses)
-            valid_losses_all.append(valid_losses)
+        hyp_params["best_weights"] = w
+        hyp_params["train_loss"] = train_loss
+        hyp_params["valid_loss"] = valid_loss
 
-    # Find the index of the minimum valid loss
-    best_loss_index = np.argmin(valid_losses_all)
-    # Get the corresponding indices for lambda and gamma
-    best_lambda_index, best_gamma_index = np.unravel_index(best_loss_index, (len(lambdas), len(gammas)))
-
-    # Retrieve the corresponding hyperparameters
-    best_lambda = lambdas[best_lambda_index]
-    best_gamma = gammas[best_gamma_index]
-
-    # Get the best weights and losses
-    best_w = w_all[best_loss_index]
-    best_train_losses = train_losses_all[best_loss_index]
-    best_valid_losses = valid_losses_all[best_loss_index]
-
-    return best_w, best_train_losses, best_valid_losses, best_lambda, best_gamma
+    return hyp_params_settings
